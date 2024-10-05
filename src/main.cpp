b18d85c5e5dcb42bb5b7ac0cc5c046a191051ba4
RtyEuzch <137963453+RtyEuzch@users.noreply.github.com>
2024-10-05 15:54:58 -0500

    Tried out filesystem::copy function with success; code used to try it is still there and will be return to later

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,18 @@
 #include <iostream>
 #include <string>
+#include <filesystem> //Might delete later?
 //#include "filehandler.h"
 using namespace std;
+namespace fs = filesystem;
 
 void gitInitialize();
 void gitPull();
 void gitPush();
 
 int main(int argc, char* argv[]) {
-    
+    // A TRIAL TEST CASE ONLY: If "test2" does not exist yet, then 
+    // it will be created.
+    fs::copy("test", "test2", fs::copy_options::recursive);
 }
 
 // Initializes a repository
